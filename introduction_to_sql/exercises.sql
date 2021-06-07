3fb2b44faa848e6c9094933d34ed2f7a5d4bac2f
Dave Millican <dave@skillerwhale.com>
2021-06-07 12:11:42 +0100

    Add exercises for the intro to SQL (#2)

---
+++ introduction_to_sql/exercises.sql
@@ -0,0 +1,100 @@
+/* -----------------------------------------------------------------------------
+|   1. SELECT
+|
+|   Write two separate SELECT queries in the area below this comment.
+|
+|   These should return:
+|       1. All rows and columns from the artists table.
+|       2. The date, and amount of all rows in the sales table.
+|
+|   Remember to include a semicolon ; after each query.
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+
+/* -----------------------------------------------------------------------------
+|   2. LIMIT / OFFSET
+|
+|   In the space below, write a query that will SELECT from the 1000th row of
+|   the sales table, up to and including the 1010th row.
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+
+/* -----------------------------------------------------------------------------
+|   3. Filtering with WHERE
+|
+|   In the space below, write three separate queries that will select:
+|
+|    * All artists whose name is not 'Arctic Monkfish'.
+|    * The title of all albums with 11 tracks.
+|    * All albums released between the start of 2020, and the 30th of June 2020.
+|
+|   Note: If you're unsure what column names are on each table, you can run a
+|         SELECT query for that table (e.g. in scratchpad.sql) to check.
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+
+/* -----------------------------------------------------------------------------
+|   4. Multiple WHERE clauses
+|
+|   In the space below, write a single query that will return all albums with:
+|
+|    * The record label `'Atlantic'`, and
+|    * A genre of `'eelectronic'` or `'sole_music'`
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+
+/* -----------------------------------------------------------------------------
+|   5. ORDER BY
+|
+|   In the space below, write a query that will return the album_id, and amount
+|   for the 10 rows in the sales table which have the largest amount.
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+
+/* -----------------------------------------------------------------------------
+|   6. Sorting by Multiple Criteria
+|
+|   In the space below, write a single query that will SELECT all columns
+|   from the albums table, sorted:
+|
+|   * alphabetically by record_label
+|   * ...then by genre if record label is the same
+|   * ...then by release_date if record_label and genre are the same,
+|        with more recent albums appearing higher up.
+|
+|   Look at the output, and check that results are sorted how you expect
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+
+/* -----------------------------------------------------------------------------
+|   7. Expressions
+|
+|   In the space below, write a query that will will SELECT from the albums
+|   table, and return the:
+|
+|   * title
+|   * average track length  (number of minutes, divided by the number of tracks)
+|
+|   for the three albums with the largest average track length.
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
