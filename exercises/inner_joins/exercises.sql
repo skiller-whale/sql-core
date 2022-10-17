4491cf775ea2514fa11bee8766403a84844e8c55
sdpryan <s.d.p.ryan@outlook.com>
2022-10-17 10:24:18 +0100

    Exercises for inner_joins module

---
+++ exercises/inner_joins/exercises.sql
@@ -0,0 +1,88 @@
+/* -----------------------------------------------------------------------------
+|   1. Data Stored Across Multiple Tables
+|
+|   In the space given below, write the following queries:
+|
+|   * SELECT all the columns of artists table
+|   * SELECT all the columns of albums table
+|
+|   Remember to include a semicolon ; after each query.
+|   Save the file after each query you write to get the results.
+|   If you write both before saving only the last query will run.
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+
+/* -----------------------------------------------------------------------------
+|   2. Using JOIN to Combine Tables
+|
+|   In the space below, write a single query that will select the album title,
+|   and the corresponding artist's name for each album in the database.
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+
+/* -----------------------------------------------------------------------------
+|   3. Filtering Results After Joining
+|
+|   In the space below, write a query that will return:
+|     * The album title
+|     * The sale date
+|
+|   for all sales of albums with more than 11 tracks.
+|
+*/ -----------------------------------------------------------------------------
+
+-- YOUR CODE GOES HERE
+
+/* -----------------------------------------------------------------------------
+|   4. Filtering vs Joins
+|
+|   Edit the following queries using appropriate JOIN and WHERE conditions
+|
+*/ -----------------------------------------------------------------------------
+
+-- This query finds any sales recorded on the day an album was released
+-- SELECT *
+-- FROM albums a, sales s
+-- WHERE s.date = a.release_date
+-- AND s.album_id = a.id;
+
+-- YOUR REWRITE GOES HERE
+
+-- This query finds all eelectronic sales recorded in 2020
+-- SELECT *
+-- FROM sales s, albums a
+-- WHERE s.date >= '2020-01-01'
+-- AND s.date < '2021-01-01'
+-- AND s.album_id = a.id
+
+-- YOUR REWRITE GOES HERE
+
+/* -----------------------------------------------------------------------------
+|   5. Joining on multiple columns
+|
+|   This question uses a new industry_revenue_history table with global information
+|   about the percentages of revenue for different music formats.
+|
+|   You can uncomment the query below to see this table.
+|   Note that the period_end date is NOT inclusive
+|   (a period goes from the period_start date until the day before the period_end)
+|
+|   Then, in the space below, write a query that will return:
+|     * The album title
+|     * The release date
+|     * What % of music industry revenue came from streaming in the period it was released in.
+|
+|   for all albums released in periods when streaming accounted for more than 50% of revenue
+|
+*/ -----------------------------------------------------------------------------
+
+-- SELECT *
+-- FROM industry_revenue_history;
+
+-- YOUR CODE GOES HERE
