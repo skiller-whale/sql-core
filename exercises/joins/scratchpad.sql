d2f95fecb4a2f08b3f89ca2938e63fe1b9814bf9
Sam Ryan <110458089+sdpryan@users.noreply.github.com>
2023-05-31 17:42:48 +0100

    Delete obsolete joins exercises (#16)
    These confused a learner today

--- exercises/joins/scratchpad.sql
+++
@@ -1,5 +0,0 @@
--- Any query you write in here will be run against the
--- database when you save the file.
---
--- The database will be reset after each query, so you don't
--- need to worry about deleting or modifying any data!
